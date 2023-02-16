c6a0fd96cf1b2c703ffc779d637c1367885bb6ab
Shuai Jiang <shuai.jiang.gr@dartmouth.edu>
2023-02-16 14:01:00 -0500

    rename files according to the order to run

--- for_vit/pipeline.sh
+++ for_vit/pipeline.sh
@@ -1,8 +1,8 @@
 # example pipeline
 
-python get_svs_meta.py
-python patch_extraction.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
-python get_patches_meta.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
-python feature_extraction.py -c=TCGA_COAD -m=10 -s=224
-python post_process.py -c=TCGA_COAD -m=10 -s=224
+python 01_get_svs_meta.py
+python 02_patch_extraction.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
+python 03_get_patches_meta.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
+python 04_feature_extraction.py -c=TCGA_COAD -m=10 -s=224
+python 05_post_process.py -c=TCGA_COAD -m=10 -s=224
 
