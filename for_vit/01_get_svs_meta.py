ec956a76c9e937ce29862ba4505a5df735915d36
Jack McMahon <jack.a.mcmahon.23@dartmouth.edu>
2023-02-21 17:48:29 -0500

    refactored json processing into util functions

--- for_vit/01_get_svs_meta.py
+++ for_vit/01_get_svs_meta.py
@@ -1,125 +1,24 @@
-import glob
-import pandas as pd
-from sklearn.model_selection import StratifiedKFold
-import os
-import json
+import argparse
 
+from utils.meta import MetaFile, GramStains, TCGA
 
-def parse_json(cancer, root_dir):
-    fname = os.path.join(root_dir, f'meta-files/{cancer}.json')
-    with open(fname) as f:
-        d = json.load(f)
-    results = []
-    for i in range(len(d)):
-        if len(d[i]) < 4:
-            continue
-        results.append([
-            d[i]['demographic']['age_at_index'],
-            d[i]['demographic']['race'],
-            d[i]['demographic']['gender'],
-            d[i]['demographic']['vital_status'],
-            d[i]['demographic'].get('days_to_death', -1),
-            d[i]['demographic']['submitter_id'],
-            d[i]['case_id'],
-            # d[i]['diagnoses'][0]['tumor_stage'],
-            d[i]['diagnoses'][0]['tissue_or_organ_of_origin'],
-            d[i]['diagnoses'][0]['days_to_last_follow_up'],
-            d[i]['diagnoses'][0].get('ajcc_pathologic_m', 'NA'),
-            d[i]['diagnoses'][0].get('ajcc_pathologic_t', 'NA'),
-            d[i]['diagnoses'][0].get('ajcc_pathologic_n', 'NA'),
-            d[i]['diagnoses'][0].get('primary_diagnosis', '--'),
-        ])
-
-    df = pd.DataFrame(results)
-    df.columns = [
-        'age_at_index',
-        'race',
-        'gender',
-        'vital_status',
-        'days_to_death',
-        'submitter_id',
-        'case_id',
-        # 'tumor_stage',
-        'tissue_or_organ_of_origin',
-        'days_to_last_follow_up',
-        'ajcc_pathologic_m',
-        'ajcc_pathologic_t',
-        'ajcc_pathologic_n',
-        'primary_diagnosis'
-    ]
-    df.submitter_id = df.submitter_id.apply(lambda x: x.split('_')[0])
-
-    df.rename(columns={'submitter_id': 'id_patient'}, inplace=True)
-
-    print(df.shape)
-    print(df.id_patient.unique().shape)
-
-    # preparing the survival outcome
-    # filtering out patients without follow-up information
-
-    df['time'] = 0
-    df.loc[
-        df.vital_status == 'Alive',
-        'time'] = df[df.vital_status == 'Alive'].days_to_last_follow_up / 365
-    df.loc[df.vital_status == 'Dead', 'time'] = [
-        np.NaN if x == '--' else int(x) / 365
-        for x in df[df.vital_status == 'Dead'].days_to_death.to_list()
-    ]
-    df['time'] = df.time - df.time.min() + 0.01
-
-    df['status'] = 0
-    df.loc[df.vital_status == 'Dead', 'status'] = 1
-    print(df.describe())
-    return df[[
-        'case_id', 'id_patient', 'vital_status', 'days_to_death', 'time',
-        'status'
-    ]]
-
+parser = argparse.ArgumentParser(description='svs/json meta file production')
+parser.add_argument('--study_name', type=str, default='gram_stains')
+parser.add_argument('--dataset_type', type=str, default='gram_stains')
+parser.add_argument('--svs_path', type=str, default='/pool2/users/jackm/dpool/data/svs')
+parser.add_argument('--json_path', type=str, default='meta-files/gram_stains.json')
+parser.add_argument('--stratify_by', type=str, default='specimen_type')
+parser.add_argument('--num_folds', type=str, default= 5)
+args = parser.parse_args()
 
 if __name__ == '__main__':
-
-    ####################################
-    # TCGA_COAD
-
-    # Parameters
-    cancer = 'TCGA_COAD'
-    site = 'Colorectal'
-    svs_root = '/pool2/data'
-    stratify_by = 'status'
-    num_folds = 5
-    ####################################
-
-    df = parse_json(cancer, './')
-    df['cancer'] = cancer
-
-    files = glob.glob(f'{svs_root}/WSI_TCGA/{site}/*/*.svs')
-    print(f"{len(files)} files found!")
-
-    df_svs = pd.DataFrame(files, columns=['svs_path'])
-    df_svs['id_patient'] = df_svs.svs_path.apply(
-        lambda x: x.split('/')[-1][0:12])
-
-    df_svs = df_svs.loc[df_svs.id_patient.isin(
-        df.id_patient)].reset_index(drop=True)
-    df_svs['id_svs'] = df_svs.svs_path.apply(
-        lambda x: x.split('/')[-1].replace('.svs', ''))
-    df_svs['cancer'] = cancer
-    df_svs['slide_type'] = df_svs.id_svs.apply(lambda x: x.split('-')[3])
-
-    # only keep the ffpe slides
-    df_svs = df_svs.loc[df_svs.slide_type.str[2] == 'Z'].reset_index(drop=True)
-    df_svs['slide_type'] = 'ffpe'
-
-    df = df.loc[df.id_patient.isin(df_svs.id_patient)].reset_index(drop=True)
-
-    skf = StratifiedKFold(n_splits=num_folds, random_state=45342, shuffle=True)
-    df['fold'] = 0
-    for i, (train_index,
-            test_index) in enumerate(skf.split(df, df[stratify_by])):
-        df.loc[test_index, 'fold'] = i
-
-    print(pd.crosstab(df.fold, df[stratify_by]))
-
-    os.makedirs('meta', exist_ok=True)
-    df.to_pickle(f'meta/{cancer.lower()}_meta.pickle')
-    df_svs.to_pickle(f'meta/{cancer.lower()}_svs.pickle')
+    
+    if (args.dataset_type == 'gram_stains'):
+        dataset = GramStains(args.study_name, args.svs_path, args.json_path
+                                , args.stratify_by, args.num_folds)
+    if (args.dataset_type == 'tcga'):
+        dataset = TCGA(args.study_name, args.svs_path, args.json_path
+                                , args.stratify_by, args.num_folds)
+    
+    dataset.split()
+    dataset.make_pickle()
