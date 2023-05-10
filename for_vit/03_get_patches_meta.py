f8a5792cea146a948defc84021bc06d372fc1d19
Jack McMahon <jack.a.mcmahon.23@dartmouth.edu>
2023-05-10 12:40:27 -0400

    fix: config based extension handling for 03_get_patches_meta

--- for_vit/03_get_patches_meta.py
+++ for_vit/03_get_patches_meta.py
@@ -50,8 +50,7 @@ def main():
             print(traceback.format_exc())
 
     patch_dir = create_patches_dir(study_name, magnification, patch_size)
-    svs_ids = [p.name.split('.')[0] for p in patch_dir.glob("*")]
-    #svs_ids = [p.name for p in patch_dir.glob("*")]
+    svs_ids = [p.name.replace(config.study.image_extension, '')for p in patch_dir.glob("*")]
     
     df = pd.concat(res)
     df = df.loc[df.id_svs.isin(svs_ids)].reset_index(drop=True)
