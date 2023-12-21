9e9b0b90b70cc1462615d70f3c6e41656877775d
Naofumi <Naofumi.Tomita@dartmouth.edu>
2023-12-21 17:38:56 -0500

    Update the pipeline script to use config file system

--- for_vit/pipeline.sh
+++ for_vit/pipeline.sh
@@ -1,9 +1,21 @@
-#TODO: OBSOLETE, NEED UPDATE THE PIPELINE. SHOULD USE SMALLER DATASET FOR TESTING.
-# example pipeline
+#!/bin/bash
 
-python 01_get_svs_meta.py
-python 02_patch_extraction.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
-python 03_get_patches_meta.py -c=TCGA_COAD --svs-meta=meta/tcga_coad_svs.pickle -m=10 -s=224
-python 04_feature_extraction.py -c=TCGA_COAD -m=10 -s=224
-python 05_post_process.py -c=TCGA_COAD -m=10 -s=224
+# Set the configuration path - Replace <<SET_YOUR_OWN_VALUE>> with the actual path to your configuration file
+CONFIG_PATH="<SET_YOUR_OWN_VALUE>"
 
+# Check if the configuration file exists
+if [ ! -f "$CONFIG_PATH" ]; then
+    echo "Configuration file not found at $CONFIG_PATH. Please check the path and try again."
+    exit 1
+fi
+
+# Running the pipeline with the specified configuration file
+echo "Running pipeline with configuration file: $CONFIG_PATH"
+
+python 01_get_svs_meta.py --user-config-file $CONFIG_PATH
+python 02_patch_extraction.py --user-config-file $CONFIG_PATH
+python 03_get_patches_meta.py --user-config-file $CONFIG_PATH
+python 04_feature_extraction.py --user-config-file $CONFIG_PATH
+python 05_post_process.py --user-config-file $CONFIG_PATH
+
+echo "Pipeline execution completed."
