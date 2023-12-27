d39927a5241c9dcf5849adc8dfa4544479e18eef
f005dg9 <f005dg9@teton.dartmouth.edu>
2023-12-26 20:57:50 -0500

    feat: added new fold generation process for IBD project

--- for_vit/datasets/base.py
+++ for_vit/datasets/base.py
@@ -5,10 +5,10 @@
 # are designed to produce self.df and self.df_svs, respectively.
 
 from pathlib import Path
-import abc
-
-import pandas as pd
 from sklearn.model_selection import StratifiedKFold, KFold
+import abc
+import numpy as np
+import pandas as pd
 
 
 class MetaFile(abc.ABC):
@@ -22,25 +22,61 @@ class MetaFile(abc.ABC):
     
     # Split dataset into k folds, with optional stratification
     def split(self):
-        if self.stratify_by is not None and self.stratify_by in self.df.columns:
-            # Use StratifiedKFold if stratification is required
-            kf = StratifiedKFold(n_splits=self.num_folds, random_state=45342, shuffle=True)
-            split_method = kf.split(self.df, self.df[self.stratify_by])
-        else:
-            # Use KFold if no stratification is required
+        np.random.seed(45342)  # fixing the seed for reproducibility
+        if self.study_name == 'IBD_PROJECT':
+            # Extract patient number from case number
+            self.df['patient_num'] = self.df.case_number.apply(lambda x: x.split(' ')[0])
+            
+            # Initialize the 'fold' column
+            self.df['fold'] = 0
+
             kf = KFold(n_splits=self.num_folds, random_state=45342, shuffle=True)
-            split_method = kf.split(self.df)
+            unique_patients = self.df['patient_num'].unique()
+            np.random.shuffle(unique_patients) 
 
-        # Initialize the "fold" column to 0
-        self.df['fold'] = 0
-        print("[INFO - split] \n", self.df.to_string())
+            # Group data by patient and get summary statistics per patient
+            patient_group = self.df.groupby('patient_num').agg({'Diagnosis': pd.Series.mode, 'patient_num': 'count'})
 
-        # Perform the split
-        for i, (_, test_index) in enumerate(split_method):
-            self.df.loc[test_index, 'fold'] = i
+            # patient group consists of most common diagnosis of WSIs for a patient and the total number of slides a patient has
+            patient_group = patient_group.rename(columns={'patient_num': 'slide_count'})
+            patient_group['Diagnosis'] = patient_group['Diagnosis'].apply(lambda x: x[0] if isinstance(x, np.ndarray) else x)
+
+            # Sort patients by the number of slides (to distribute heavy patients evenly)
+            patient_group = patient_group.sort_values(by='slide_count', ascending=False)
+
+            # Distribute patients to folds, aiming to balance both patient count and diagnosis
+            fold_diagnosis_counts = {i: {} for i in range(self.num_folds)}
+            for patient, data in patient_group.iterrows():
+                # Choose the fold with the least representation of this patient's most common diagnosis
+                least_rep_fold = min(fold_diagnosis_counts, key=lambda x: fold_diagnosis_counts[x].get(data['Diagnosis'], 0))
+                # Assign this patient to that fold
+                self.df.loc[self.df['patient_num'] == patient, 'fold'] = least_rep_fold
+                # Update fold diagnosis count
+                fold_diagnosis_counts[least_rep_fold][data['Diagnosis']] = fold_diagnosis_counts[least_rep_fold].get(data['Diagnosis'], 0) + data['slide_count']
+
+        else:
+            if self.stratify_by is not None and self.stratify_by in self.df.columns:
+                # Use StratifiedKFold if stratification is required
+                kf = StratifiedKFold(n_splits=self.num_folds, random_state=45342, shuffle=True)
+                split_method = kf.split(self.df, self.df[self.stratify_by])
+            else:
+                # Use KFold if no stratification is required
+                kf = KFold(n_splits=self.num_folds, random_state=45342, shuffle=True)
+                split_method = kf.split(self.df)
+
+            # Initialize the "fold" column to 0
+            self.df['fold'] = 0
+            # print("[INFO - split] \n", self.df.to_string())
+
+            # Perform the split
+            for i, (_, test_index) in enumerate(split_method):
+                self.df.loc[test_index, 'fold'] = i
 
         # Optional: Display the distribution across folds
         print("[INFO - split] Distribution of data across folds:\n", self.df['fold'].value_counts())
+        
+        for fold in range(self.num_folds):
+            print(f"Fold {fold} diagnosis distribution:\n", self.df[self.df['fold'] == fold]['Diagnosis'].value_counts())
 
     
     #Produces meta.pickle and svs.pickle files for dataset in specific directory
